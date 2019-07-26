03119c5c3cbef89faeff23b0e9f470678c6ea834
Geraldvilladores21 <44021057+Geraldvilladores21@users.noreply.github.com>
2019-07-26 14:52:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function light(sw) {
     var pic;
     if (sw == 0) {
-      pic = "pic_bulboff.gif"
+      pic = "img/pic_bulboff.gif"
     } else {
-      pic = "pic_bulbon.gif"
+      pic = "img/pic_bulbon.gif"
     }
     document.getElementById('myImage').src = pic;
   }
